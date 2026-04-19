7cc31983f0c71668e635b2694e7c40ef0a360e3c
krameraad <eliaspowder@gmail.com>
2026-04-19 14:10:05 +0200

    small reformatting

--- builder.py
+++ builder.py
@@ -2,12 +2,15 @@ from zone import Zone
 
 
 def build(data: dict) -> dict[str, Zone]:
+    """Creates a dictionary containing all necessary objects for the map.
+    """
     start_hub = Zone(**data['start_hub'])
     end_hub = Zone(**data['end_hub'])
-    zones: dict[str, Zone] = {x['name']: Zone(**x) for x in data['hubs']}
-    zones.update({start_hub.name: start_hub, end_hub.name: end_hub})
+
+    hubs: dict[str, Zone] = {x['name']: Zone(**x) for x in data['hubs']}
+    hubs.update({start_hub.name: start_hub, end_hub.name: end_hub})
 
     for link in data['links']:
-        zones[link[0]].links.append((zones[link[1]], link[2]))
-        zones[link[1]].links.append((zones[link[0]], link[2]))
-    return zones
+        hubs[link[0]].links.append((hubs[link[1]], link[2]))
+        hubs[link[1]].links.append((hubs[link[0]], link[2]))
+    return hubs
